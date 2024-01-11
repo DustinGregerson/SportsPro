8a802d8e367007e1b2ecf24e54bd5a60375c7cae
DustinGregerson <dustin.gregerson@southeasttech.edu>
2024-01-11 11:12:28 -0600

    Added admin Area it doesnt do anything yet. Added the List view for the product controller and built a crude for loop that displays the name for all the products in the List view.

---
+++ SportsPro/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsPro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private SportsProContext context;
+        public ProductController(SportsProContext ctx)
+        {
+           context=ctx;
+        }
+
+        public IActionResult List()
+        {
+            List<Product> products;
+            products = context.Products.ToList();
+
+            return View(products);
+        }
+    }
+}
