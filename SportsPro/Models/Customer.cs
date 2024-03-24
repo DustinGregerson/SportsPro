3330295220e17fb269ef06fbc6514130abb07c18
firaol81 <firaol.baneta@southeasttech.edu>
2024-03-24 03:11:35 -0500

    Validation

--- SportsPro/Models/Customer.cs
+++ SportsPro/Models/Customer.cs
@@ -7,35 +7,35 @@ namespace SportsPro.Models
     {
 		public int CustomerID { get; set; }
 
-		[Required(ErrorMessage ="Please enter first name.")]
+		[Required(ErrorMessage ="Required.")]
         [MaxLength(51)]
         [MinLength(1)]
         [RegularExpression("^[a-zA-Z]+$", 
 			ErrorMessage = "First Name must not contain special characters or numbers")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter Last name.")]
+        [Required(ErrorMessage = "Required")]
         [MaxLength(51)]
         [MinLength(1)]
         [RegularExpression("^[a-zA-Z]+$",
             ErrorMessage = "Last Name must not contain special characters or numbers")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please Your Address.")]
+        [Required(ErrorMessage = "Required.")]
         [MaxLength(51)]
         [MinLength(1)]
         [RegularExpression("^([a-zA-Z0-9\\s])+$",
             ErrorMessage = "Address must not contain special characters")]
         public string Address { get; set; }
 
-        [Required(ErrorMessage = "Please enter City.")]
+        [Required(ErrorMessage = "Required.")]
         [MaxLength(51)]
         [MinLength(1)]
         [RegularExpression("^[a-zA-Z\\s]+$",
             ErrorMessage = "City must not contain special characters or numbers")]
         public string City { get; set; }
 
-        [Required(ErrorMessage = "Please enter State.")]
+        [Required(ErrorMessage = "Required.")]
         [RegularExpression("^[A-Z]{2}$",
             ErrorMessage = "State must be abbreviated and contain capital letters ie.SD")]
         public string State { get; set; }
@@ -46,19 +46,25 @@ namespace SportsPro.Models
         [RegularExpression("^[0-9]+$", ErrorMessage = "postal code must be numeric")]
         public string PostalCode { get; set; }
 
-        [Required(ErrorMessage = "Please pick a country.")]
+        [Required(ErrorMessage = "Required.")]
         public string CountryID { get; set; }
 		public Country Country { get; set; }
 
-        [RegularExpression(@"^\([1-9][0-9][0-9]\)\s[0-9]{3}-[0-9]{4}$",
-                   ErrorMessage = "Entered phone format is not valid. (999) 999-9999")]
+       [Required(ErrorMessage = "Phone number must be in (999) 999-999 format.")]
+        [MaxLength(14, ErrorMessage = "Phone number cannot be more than 14 characters.")]
+        [MinLength(14, ErrorMessage = "Phone number must be in (999) 999-999 format.")]
+        [RegularExpression(@"^\(\d{3}\)\s*\d{3}-\d{4}$", ErrorMessage = "Phone number must be in (999) 999-9999 format.")]
         public string Phone { get; set; }
 
-        [Required(ErrorMessage = "Please enter an Email.")]
+        [Required(ErrorMessage = "Please enter a valid email address.")]
         [StringLength(51)]
         [DataType(DataType.EmailAddress, ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
 
-		public string FullName => FirstName + " " + LastName;   // read-only property
+
+   
+
+
+        public string FullName => FirstName + " " + LastName;   // read-only property
 	}
 }
